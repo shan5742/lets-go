952b716fe5f2091d751e376b1327d43eb1fcf7d8
Asam Shan <asamshan456@gmail.com>
2019-05-21 10:29:06 +0100

    adding instructionss and basic structure of problem 6

---
+++ 06_fscanf/main.go
@@ -0,0 +1,21 @@
+/*
+Problem #6: The fscanf function
+Author: Asam Shan
+Date Created: 21/05/2019
+*/
+
+/*
+***************** The fcsanf function *****************
+Using your favourite editor, create file called testdata6 that has a single integer value stored into it. Write a program
+that opens a file named testdata6, reads the single value stored in it using the fscanf function, and then prints the
+value back onto the screen. You should call the function fclose before you return to the Unix operating system. Read
+about the function fclose for a better understanding of it.
+*/
+
+package main
+
+func main() {
+
+	// TODO add solution
+
+}
