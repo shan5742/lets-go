a8f20cbd09b9267ec7e84a2a69eafe45809955d8
Asam Shan <asamshan456@gmail.com>
2019-05-21 09:48:31 +0100

    adding folder and basic file structure for problem 4

---
+++ 04_ScanF/main.go
@@ -0,0 +1,20 @@
+/*
+Problem #4: The Scan F function
+Author: Asam Shan
+Date Created: 21/05/2019
+*/
+
+/*
+***************** The Scan F function *****************
+Write a program that reads in an integer value from the keyboard via the scanf function and then prints it back
+onto the screen. You should know that scanf seeks an address expression. For example, &n is an address expression.
+You should also know that scanf returns a value. It is either the number of successful conversions or an error code
+that is typically EOF.
+*/
+
+package main
+
+func main() {
+	/* TODO add fucntion */
+
+}
